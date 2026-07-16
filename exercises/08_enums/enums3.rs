9e7772786388959713102b47768b5b7178e965e9
Jonathan LOQUET <8999079+JonathanLT@users.noreply.github.com>
2026-07-16 18:42:10 +0200

    Rustlings: enums3 OK (#37)

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,23 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::Quit => {
+                self.quit();
+            },
+            Message::Move(Point { x, y }) => {
+                self.move_position(Point { x, y });
+            },
+            Message::Echo(text) => {
+                self.echo(text);
+            },
+            Message::ChangeColor(r, g, b) => {
+                self.change_color(r, g, b);
+            },
+            Message::Resize { width, height } => {
+                self.resize(width, height);
+            }
+        }
     }
 }
 
